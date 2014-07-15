f85deff9dedcf8701a887f73f03d87f260da967d
JimberXin <newton110@qq.com>
2014-07-15 22:19:20 +0800

    update

---
+++ PartionList.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+struct ListNode
+{
+	   int val;
+	   ListNode* next;
+	   ListNode(int x): val(x), next(NULL){}
+};
+
+class Solution {
+public:
+    ListNode *partition(ListNode *head, int x)
+	{
+            ListNode* pSmall =  (ListNode*)malloc(sizeof(ListNode)) ;
+			ListNode* pBig     =  (ListNode*)malloc(sizeof(ListNode));
+            ListNode* pt = head;
+			ListNode* small_head = pSmall;
+			ListNode* big_head = pBig;
+			if( NULL == head || NULL ==  head -> next)
+				  return NULL;
+			while(NULL != pt)
+			{
+				  if(pt -> val < x)
+				  {
+					   pSmall -> next = pt;
+					   pSmall = pSmall -> next;
+				  }
+				  else
+				  {
+					   pBig -> next = pt;
+					   pBig = pBig -> next;
+				  }
+				  pt = pt -> next;
+			}
+			 pSmall -> next = ( big_head == pBig)? NULL: big_head -> next;   //judge whether big_head has elements
+			 pBig -> next = NULL;
+			 return small_head -> next;
+    }
+};
+
+
+int main()
+{
+	  //int num[] =  {8,2,3,45,5,6,13,9,12,32,0,35};
+	  int num[] = {4,1,0,2,2,2,2,4,0,4,2,3,1};
+	  Solution s;
+	  ListNode* pt, *current;
+	  pt = (ListNode*)malloc(sizeof(ListNode));
+	  current = pt;
+	  int i;
+	  for(i=0;i<sizeof(num)/sizeof(int);i++)
+	  {
+		     ListNode* tmp =  (ListNode*) malloc(sizeof(ListNode));
+			  tmp -> val = num[i];
+			  current  -> next  = tmp;
+			  current =  current -> next;
+	  }
+	    current -> next  = NULL;
+   
+	  ListNode* newhead = s.partition(NULL, 7);
+	
+	  while(NULL != newhead)
+	  {
+		  cout <<  newhead -> val << " " ;
+		  newhead =  newhead -> next;
+	  }
+	  cout <<    endl;
+      
+
+	  getchar();
+	  return 0;
+}
---
+++ mergeIntervals.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+struct Interval
+{
+	  int start;
+	  int end;
+	  Interval(): start(0),end(0){}
+	  Interval(int s, int e): start(s),end(e){}
+};
+
+class Compare
+{
+public:
+	   bool  operator()(const Interval& I1, const Interval& I2)
+		 {
+			 return I1.start < I2.start? true:false;
+		 }
+};
+
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval> &intervals) 
+    {
+        //
+		    /* vector<Interval> result;
+			 if (intervals.size() == 0 || intervals.size() == 1)
+				   return intervals;
+			 sort(intervals.begin(),intervals.end(),Compare());
+			 vector<Interval>::iterator current, next;
+			 current = intervals.begin();
+			 
+			 while( (current+1) != intervals.end() )
+			 {
+				   next = current +1;
+				   if( next->start  <= current -> end && next ->end <= current -> end )
+					   intervals.erase(next);
+				   else if(next -> start  <= current -> end && next -> end >= current -> end)
+				        {
+							     current -> end = next -> end;
+								 intervals.erase(next);
+				        }
+				   else
+					   current  = next;
+			 }
+			 return intervals;*/
+		     vector<Interval> result;
+			 if (intervals.size() == 0 || intervals.size() == 1)
+				   return intervals;
+			 sort(intervals.begin(),intervals.end(),Compare());
+			result.clear();
+			result.push_back(intervals[0]);
+			 for(int i=1; i<intervals.size();i++)
+			 {
+						   int size = result.size();
+						   if( result[size-1].end >= intervals[i].start )
+							   result[size-1].end = max(result[size-1].end,intervals[i].end);
+						   else
+							   result.push_back(intervals[i]);
+			 }
+			 return result;             
+        //  
+    }
+};
+
+int main()
+{
+	Solution s;
+	vector<Interval> res;
+	int arr[] ={1,2,2,3,3,4,6,7,8,9,13,20,14,19,16,23};
+	for(int i=0;i<sizeof(arr)/sizeof(int);i++)
+	{
+		  Interval temp(arr[i],arr[i+1]);
+		  res.push_back(temp);
+		  i++;
+		  
+	}
+	
+	vector<Interval> result  = s.merge(res);
+	getchar();
+	return 0;
+}
+	
