696391a2e4f716c873e34ad8d3f0bf187c2c4558
JimberXin <newton110@qq.com>
2014-07-17 23:22:36 +0800

    Revert "Revert "Revert "Revert "Revert "Revert "Update Leetcode""""""
    This reverts commit 114500358b08d798a7ee0b22267fa99143a80960.

---
+++ buildeTreeFreomInandPRE.cpp
@@ -0,0 +1,141 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+struct TreeNode
+{ 
+	 int val;
+	 TreeNode* left;
+	 TreeNode* right;
+	 TreeNode(int x): val(x), left(NULL),right(NULL){}
+};
+class Solution {
+public:
+	//unsigned int count;
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder)
+    {
+        //
+		 //TreeNode* root;
+		 //  count ++;
+		 //TreeNode* node= (TreeNode*) malloc(sizeof(TreeNode));
+		 //if( preorder.empty() )
+			//  return NULL;
+		 //  vector<int>::iterator pre,in;
+		 //  pre = preorder.begin();  //find the root in preorder
+		 //  in = find(inorder.begin(),inorder.end(),*pre); //find the root in inorder
+		 //  	 node -> val = *pre;
+		 //if(preorder.size() == 1)
+		 //{
+		 //      node -> left = node -> right = NULL;
+			//  // return node;
+		 //}
+		 //   //*******************************no left child****************************************
+		 //else    if( in == inorder.begin())
+		 //   {
+			//	vector<int> preRight(++pre, preorder.end());
+			//	vector<int>inRight(++in,inorder.end());
+			//     node -> left  = NULL;
+			//	 node -> right = buildTree(preRight, inRight);
+		 //    }  
+			//  //**************************************no  right child***********************************
+		 //  else   if( *in == inorder.back() )
+			//  {
+			//	   vector<int> preLeft(++pre,preorder.end());
+			//	   vector<int> inLeft(inorder.begin(), in); //not  --in
+			//	   node -> right =  NULL;
+			//	   node -> left = buildTree(preLeft,inLeft);
+			//  }
+		 //   //******************both left child an right childe exist***********************************
+		 //  else 
+			//  {
+			//	  //should use the numbers, not the iterator
+		 //       //in_pre = find(preorder.begin(),preorder.end(),*(--in)); //find the middle of the preorder
+			//	     vector<int>::iterator it2 = preorder.begin();
+			//	  for(vector<int>::iterator it1= inorder.begin();  it1 != in;  it1++,it2++)  ;  //it2 marks the middle of preorder
+		 //        vector<int> inLeft(inorder.begin(),in); //in has becomes the last element of the left after --
+		 //        vector<int> inRight(++in,inorder.end());
+		 //        vector<int> preLeft(++pre,++it2);
+		 //        vector<int> preRight(it2, preorder.end());
+
+		 //         node -> left =  buildTree(preLeft, inLeft);
+		 //         node -> right =  buildTree(preRight,inRight);
+			//  }
+		 //return node;		       
+        //      
+		return 0 == preorder.size()?NULL:createTreeTemp(preorder.begin(),preorder.end(),inorder.begin(),inorder.end());
+
+    }
+	TreeNode* createTreeTemp(vector<int>::iterator preL,vector<int>::iterator preR, vector<int>::iterator inL, vector<int>::iterator inR)
+	{
+		   if(preL >= preR) 
+			       return NULL;
+		   int value = *preL;
+		   TreeNode *root = new TreeNode(value);
+		   vector<int>::iterator in = find(inL,inR,value);
+		   vector<int>::size_type num = in - inL;
+
+		   root -> left = createTreeTemp(preL+1,preL+num+1,inL,in);
+		   root -> right = createTreeTemp(preL+num+1,preR,in+1,inR);
+
+		   return root;
+	}
+
+public:
+	void preorderTraversal(TreeNode *root)
+	{
+		   if(NULL != root)
+		   {
+			      cout << root -> val << " " ;
+				  preorderTraversal( root -> left);
+				  preorderTraversal( root -> right);
+
+		   }
+	}
+
+	void inorderTraversal(TreeNode* root)
+	{
+		if(NULL != root)
+		 {		      
+				  inorderTraversal( root -> left);
+				  cout << root -> val << " " ;
+				  inorderTraversal( root -> right);
+
+		   }
+	}
+};
+
+
+int main()
+{ 
+	Solution s;
+	 vector<int> pre, in;
+	// int a[]= {3,2,1,4};
+	 //int b[]= {1,2,3,4};
+	 int a[] = {9,-6,7,5,3,8,11};
+	 int b[] = {7,-6,5,9,8,3,11};
+	 int length = sizeof(a)/sizeof(int);
+	 for(int i=0;i<length;i++)
+	 {
+		  pre.push_back(a[i]);
+		   in.push_back(b[i]);
+	 }
+		TreeNode* root = s.buildTree(pre,in);
+		cout << "Preorder of the created tree is: ";
+		s.preorderTraversal(root);
+		cout << endl <<  "Inorder of the created tree is: ";
+		s.inorderTraversal(root);
+		getchar();
+		  return 0;
+}
---
+++ combinationSum.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+// reference of mannual c++ pp.156-pp.157
+class Solution {
+public:
+    vector<vector<int> > combinationSum(vector<int> &candidates, int target)
+    {
+        //
+		sort(candidates.begin(),candidates.end());
+          vector<vector<int> > result;
+		  vector<int> temp;
+		  DFS(candidates,target,0,temp,result);
+		  return result;
+     
+        //
+        
+    }
+
+	void DFS(vector<int> nums, int gap, int current, vector<int> &temp, vector<vector<int> > &result)
+	{
+		//     find onde solution
+		if( gap == 0)
+		{ 
+			result.push_back(temp);
+			return;
+		}
+		for(size_t i=current;i<nums.size();i++)
+		{
+			    if(gap < nums[i])
+					  return ;  // cut it down
+				temp.push_back(nums[i]);
+				DFS(nums,gap-nums[i],i,temp,result);
+				temp.pop_back();
+		}
+		   
+	}
+};
+
+int main(void)
+{
+	Solution s;
+	int arr[] = {2,7,6,3};
+	vector<int> num;
+	vector< vector<int> > result;
+	for(int i=0; i<sizeof(arr)/sizeof(int);i++)
+		num.push_back(arr[i]);
+	result = s.combinationSum(num,7);
+	vector<vector<int> > :: iterator it;
+	for(it = result.begin(); it != result.end(); it++)
+	{
+		  for(vector<int>::iterator inside = it->begin(); inside != it->end(); inside++)
+			    cout << *inside << " " ;
+		    cout << endl;
+	}
+	getchar();
+	return 0;
+}
