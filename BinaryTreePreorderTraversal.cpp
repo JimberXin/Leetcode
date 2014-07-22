db944a84fac58816b4e41dc17cac6d2ee22b291e
JimberXin <newton110@qq.com>
2014-07-22 12:14:43 +0800

    update

---
+++ BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+
+using namespace std;
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+struct TreeNode
+{
+	   int val;
+	   TreeNode *left;
+	   TreeNode *right;
+	   TreeNode(int x): val(x), left(NULL), right(NULL){}
+};
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode *root)
+	{
+         //
+		      vector<int> values;
+			  stack<TreeNode*> BSstack;
+			 while( NULL != root || !BSstack.empty())
+			 {
+				       while(NULL != root)
+					   {
+						    // cout << root -> val << " ";
+							 values.push_back(root -> val);
+							 BSstack.push(root);
+							 root = root -> left;
+					   }
+
+					     root = BSstack.top();
+						 BSstack.pop();
+						 root =  root -> right;
+			 }
+			 return values;
+
+		//
+    }
+
+	public:
+	TreeNode* createTree(void)
+	{    
+		  int data; 
+		  TreeNode* root;
+		  cin >> data;
+		   if( data == -1)
+			    root = NULL;
+		   else
+		   { 
+			     root = (TreeNode*)malloc(sizeof(TreeNode));
+			     root -> val = data;
+				 root -> left  = createTree();
+				 root -> right = createTree();
+		   }
+		   return root;
+	}
+
+};
+
+int main()
+{
+	Solution s;
+	TreeNode* root =  s.createTree();
+	  vector<int> pre = s.preorderTraversal(root);
+	  for(vector<int>::iterator it = pre.begin(); it != pre.end(); it++)
+		     cout << *it << " " ;
+	 
+	cin.get();
+	getchar();
+	return 0;
+}
---
+++ sortedArrayToBST.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+struct TreeNode
+{
+	 int val;
+	 TreeNode *left;
+	 TreeNode *right;
+	 TreeNode(int x):val(x),left(NULL),right(NULL){}
+};
+
+class Solution {
+public:
+    TreeNode *sortedArrayToBST(vector<int> &num)
+    {
+        //
+          return buildBST(num,0,num.size()-1);
+        //  
+    }
+
+	TreeNode *buildBST(vector<int>&num, int begin, int end)
+	{
+		     if(begin > end)
+				 return NULL;
+			 int mid = begin + (end - begin)/2;
+			 TreeNode* left = buildBST(num,begin,mid-1);
+			 TreeNode* right = buildBST(num,mid+1,end);
+			 TreeNode *root = new TreeNode(num[mid]);
+			 root -> left = left;
+			 root -> right = right;
+			 return root;
+	}
+};
+
+int main()
+{
+	Solution s;
+	vector<int> num;
+	int arr[] ={ 1,2,3,4,5};
+	for(int i=0; i<sizeof(arr)/sizeof(int);i++)
+		num.push_back(i);
+
+	TreeNode *root = s.sortedArrayToBST(num);
+
+	getchar();
+	return 0;
+}
