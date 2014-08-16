e62c8f2c8ba51e9d5ab284ccdd224a055b1ce045
JimberXin <newton110@qq.com>
2014-08-16 14:05:05 +0800

    updated

---
+++ Word Break II.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+#include<set>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+	void DFS(string s, int index, vector<string> &result, vector<string> &temp, 
+		                unordered_set<string> &dict,vector<vector<bool> > isMatch)
+	{
+		int n = s.length();
+		   if(index >= n ){
+			      string ret;
+			      for(int i =0; i < temp.size(); i++)
+					 ret += temp[i] + " " ;
+				  ret.pop_back();
+				  result.push_back(ret);
+		   }
+
+		   for(int len = 1; len <= n - index; len++)
+		      {
+			     string prefixstr = s.substr(index,len);
+			     if( dict.count(prefixstr) >0 &&  ( (index+len) == n || isMatch[n-index-len][index+len] ) )
+			         {
+				        temp.push_back(prefixstr);
+				        DFS(s, index+len, result, temp, dict, isMatch);
+				        temp.pop_back();
+			        }
+		      }
+	}
+
+    vector<string> wordBreak(string s, unordered_set<string> &dict) {
+		//
+		int n = s.length();
+		vector<vector<bool> > isMatch(n+1, vector<bool>(n+1, false)); //m[len][start]:from the index:start, len numbers
+		int maxLen, minLen;
+		unordered_set<string>::iterator it = dict.begin();
+        for(int i=0; i <= n; i++)
+			isMatch[0][i] = false;
+		for(int len = 1; len <= n; len++){
+			for(int start = 0; start <= n - len; start++){
+				     if(dict.count(s.substr(start,len)) >0 )
+						 isMatch[len][start] = true;
+					 else
+					 {
+						   for(int k = 1; k < len; k++){
+							     isMatch[len][start] = isMatch[k][start] && isMatch[len-k][start+k];
+								   if( isMatch[len][start] ) break;
+						   }
+					 }
+			   }
+		}
+
+	    	vector<string> result;
+			vector<string> temp;
+	    	if(!isMatch[n][0]) 
+			   return result;
+		     DFS(s,0,result,temp, dict,isMatch);
+			 return result;
+		//
+    }
+};
+
+int main(void){
+	Solution s;
+	string arr1[] = {"cat", "cats", "and", "sand", "dog"};
+	string str = "catsanddog";
+	unordered_set<string> dictinary;
+	for(int i =0 ; i < 5; i++)
+		dictinary.insert(arr1[i]);
+	vector<string> res = s.wordBreak(str,dictinary);
+	getchar();
+	return 0;
+}
---
+++ Word Break.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<set>
+#include<algorithm>
+#include<unordered_set>
+using namespace std;
+
+class Solution {
+public:
+	bool  wordBreakHelper(string s, unordered_set<string> &dict, set<string> &unmathed, int maxL, int minL){
+		     if(s.empty()) return true;
+			 int i = maxL > s.length()? s.length() : maxL;
+			 for(; i > 0 ; i--)
+			 {
+				       string preffixstr = s.substr(0,i);
+					   if( dict.find(preffixstr) != dict.end() ){
+						   string suffixstr = s.substr(i);
+						      if(unmathed.find(suffixstr) != unmathed.end() )  continue;
+							  else 
+								   if(wordBreakHelper(suffixstr,dict,unmathed,maxL,minL) )
+									    return true;
+								   else
+									    unmathed.insert(suffixstr);
+					   }
+			 }
+			    return false;
+	}
+
+    bool wordBreak(string s, unordered_set<string> &dict) {
+          if(s.length() == 0) return true;
+		  if(dict.empty()) return false;
+		  unordered_set<string>::iterator it = dict.begin();
+		   int maxLen = (*it).length(), minLen = (*it).length();
+		   for(; it != dict.end(); it++)
+		   {
+			      if((*it).length() > maxLen)
+					   maxLen = (*it).length();
+				  else if((*it).length() < minLen)
+					  minLen = (*it).length();
+		   }
+		   set<string> unmathed;  // records the words that are not matched.
+		   return wordBreakHelper(s,dict,unmathed,maxLen,minLen);
+    }
+};
+
+int main(void){
+	Solution s;
+	string str = "leetcode";
+	unordered_set<string> dictinary;
+	dictinary.insert("leet");
+	dictinary.insert("cod");
+	bool isTrue = s.wordBreak(str,dictinary);
+	cout << isTrue << endl;
+	getchar();
+	return 0;
+}
+
+
+//
+//bool wordBreak(string s, unordered_set<string> &dict) {  
+//    vector<bool> wordB(s.length() + 1, false);  
+//    wordB[0] = true;  
+//    for (int i = 1; i < s.length() + 1; i++) {  
+//        for (int j = i - 1; j >= 0; j--) {  
+//            if (wordB[j] && dict.find(s.substr(j, i - j)) != dict.end()) {  
+//                wordB[i] = true;  
+//                break; //只要找到一种切分方式就说明长度为i的单词可以成功切分，因此可以跳出内层循环。  
+//            }  
+//        }  
+//     }  
+//    return wordB[s.length()];  
+//   }  
