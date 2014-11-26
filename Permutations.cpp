01ee54b7e9594f2a17d8bade5e444d5e8fb7900c
Junbo Xin <jimberxin@gmail.com>
2014-11-27 01:39:49 +0800

    update leetcode

---
+++ Permutations.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int> > permute(vector<int> &num) 
+    {
+        //
+        
+        
+        
+        //       
+    }
+};
