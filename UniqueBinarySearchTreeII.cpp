3ff35bf3a89cfbb170ad20bec60bc63791768d2c
Junbo Xin <jimberxin@gmail.com>
2014-12-02 00:04:01 +0800

    fixed the format of space

--- UniqueBinarySearchTreeII.cpp
+++ UniqueBinarySearchTreeII.cpp
@@ -14,60 +14,58 @@ using namespace std;
  */
 struct TreeNode
 {
-	  int val;
-	  TreeNode *left;
-	  TreeNode *right;
-	  TreeNode(int x): val(x),left(NULL),right(NULL){}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x): val(x),left(NULL),right(NULL){}
 };
 
 class Solution {
 public:
-    vector<TreeNode *> generateTrees(int n) 
-	{
-		 vector<TreeNode *> result;
-		 result = buildBST(0,n-1);
+  vector<TreeNode *> generateTrees(int n) 
+  {
+    vector<TreeNode *> result;
+    result = buildBST(0,n-1);
 
-        return result;
+    return result;
     }
 
-	vector<TreeNode*>  buildBST(int begin,int end)
-	{
-		    vector<TreeNode*> Tree ;
-			//****************No elements***********************
-			if(begin > end)
-			{
-				Tree.push_back (NULL);
-				return Tree;
-			}
-			 //************More than one elements******************
-			 else
-			 {
-			          for(int i=begin; i <=  end; i++)
-			       {
-					 vector<TreeNode*> left =  buildBST(begin,i-1);
-					 vector<TreeNode*>  right =  buildBST(i+1,end);
-					  for(int j = 0; j<left.size();j++)
-					    {
-						   for(int k=0; k< right.size();k++)
-						    {
-					             TreeNode* root =  new TreeNode(i+1);
-								 Tree.push_back (root);
-								 root -> left = left[j];      //j elements in left
-								 root -> right  = right[k]; // k elements in right
-						    }
-					     }				     
-			         }	
-					  			 return Tree;
-			 }
-
-
-	}
+  vector<TreeNode*>  buildBST(int begin,int end)
+  {
+    vector<TreeNode*> Tree ;
+    //****************No elements***********************
+    if(begin > end)
+      {
+	Tree.push_back (NULL);
+	return Tree;
+      }
+    //************More than one elements******************
+    else
+      {
+	for(int i=begin; i <=  end; i++)
+	  {
+	    vector<TreeNode*> left =  buildBST(begin,i-1);
+	    vector<TreeNode*>  right =  buildBST(i+1,end);
+	    for(int j = 0; j<left.size();j++)
+	      {
+		for(int k=0; k< right.size();k++)
+		  {
+		    TreeNode* root =  new TreeNode(i+1);
+		    Tree.push_back (root);
+		    root -> left = left[j]; //j elements in left
+		    root -> right  = right[k]; // k elements in right
+		  }
+	      }				     
+	  }	
+	return Tree;
+      }
+  }
 };
 
 int main(void)
 {
-	Solution s;
-	vector<TreeNode*> result = s.generateTrees(5);
-	getchar();
-	return 0;
+  Solution s;
+  vector<TreeNode*> result = s.generateTrees(5);
+  getchar();
+  return 0;
 }
--- UniqueBinarySearchTrees.cpp
+++ UniqueBinarySearchTrees.cpp
@@ -4,30 +4,28 @@ using namespace std;
 
 class Solution {
 public:
-    int numTrees(int n) 
-    {
-		//
-		if(n < 2) 
-			  return n;
-	     vector<int> f(n+1,1);
-		 f[1] = 1;
-		 f[2] = 2;
-		 for(int i=3; i <= n;i++)
-		 {
-			  int sum = 0;
-			  for(int j=0; j < i; j++)
-				  sum += f[j] * f[i-j-1];
-			  f[i] = sum;
-		 }
-		//
-        return f[n];
-    }
+  int numTrees(int n) 
+  {
+    if(n < 2) 
+      return n;
+    vector<int> f(n+1,1);
+    f[1] = 1;
+    f[2] = 2;
+    for(int i=3; i <= n;i++)
+      {
+	int sum = 0;
+	for(int j=0; j < i; j++)
+	  sum += f[j] * f[i-j-1];
+	f[i] = sum;
+      }
+    return f[n];
+  }
 };
 
 int main(void)
 {
-	 Solution s;
-	 cout << s.numTrees(5) << endl;
-	 getchar();
-	 return 0;
+  Solution s;
+  cout << s.numTrees(5) << endl;
+  getchar();
+  return 0;
 }
