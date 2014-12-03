cc3ead5823610acf9fa98578405072dc6b5c5318
Junbo Xin <jimberxin@gmail.com>
2014-12-03 23:22:58 +0800

    update format of space

--- PathSumII.cpp
+++ PathSumII.cpp
@@ -13,115 +13,99 @@ using namespace std;
  * };
  */
 
-struct TreeNode
-{
-	  int val;
-	  TreeNode *left;
-	  TreeNode* right;
-	  TreeNode(int x):val(x), left(NULL),  right(NULL){}
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode* right;
+  TreeNode(int x):val(x), left(NULL),  right(NULL){}
 };
 
-//My solution uses the postorder traversal of the tree, uses last to mark the last nodes that I visit, and use two stacks:
+//My solution uses the postorder traversal of the tree,
+//uses last to mark the last nodes that I visit, and use two stacks:
 //nodeStack to save the nodes, sumStack to save the current passing nodes' sum.
 class Solution {
 public:
-    vector<vector<int> > pathSum(TreeNode *root, int sum) 
-    {
-        //
-                 stack<TreeNode*> nodeStack;
-				 stack<int> sumStack;
-				 int add = 0;
-				 TreeNode* last =  NULL;
-				 vector< vector<int> > all;
-				 vector<int> nodes;
+    vector<vector<int> > pathSum(TreeNode *root, int sum) {
+      stack<TreeNode*> nodeStack;
+      stack<int> sumStack;
+      int add = 0;
+      TreeNode* last =  NULL;
+      vector< vector<int> > all;
+      vector<int> nodes;
 
-				 while( NULL != root || !nodeStack.empty( ) )
-				 {
-					    while(NULL != root)
-						{
-							   nodeStack.push(root);
-							   add += root -> val;
-							   sumStack.push(add);
-							   root = root -> left;
-						}
-						  root = nodeStack.top();
-						  add = sumStack.top();
-				 
-				 if(NULL == root -> right || last == root-> right)
-				 {
-					   last = root;
-					   //deal with the case that the root-leaf path equals sum
-					   if(NULL == root -> left && NULL == root -> right && sum == add)
-					   {
-						   	      stack<TreeNode*> temp;
-						    while(!nodeStack.empty())
-						     {		
-							    TreeNode* nodetemp = nodeStack.top();
-							    temp.push(nodetemp);
-							    nodeStack.pop();
-						     }
-						    while(!temp.empty())
-							  {
-								   TreeNode* nodetemp = temp.top();
-								   nodes.push_back(nodetemp->val);
-								   nodeStack.push(nodetemp);
-								   temp.pop();
-							   }
-							all.push_back(nodes);
-							nodes.clear();
-					   }
-					   //visit this node root
-					   nodeStack.pop();
-					   sumStack.pop();
-					   root = NULL;				   
-				 }
-				 else
-					 root =  root ->right;
-
-				 }
-				// all.push_back(nodes);
-				 return all;
-
-           
-        //
-        
-    }
-
-	public:
-	TreeNode* createTree()
-	{
-		   int data;
-		   TreeNode* root;
-		   cin >> data;
-		   if(data ==  -1)
-			    root = NULL;
-		   else
-		   {
-		    root = (TreeNode*) malloc(sizeof(TreeNode));
-			root -> val =  data;
-			root -> left =  createTree();
-			root -> right = createTree();
-		   }
-			return root;
-			     
+      while( NULL != root || !nodeStack.empty( ) ){
+	while(NULL != root) {
+	  nodeStack.push(root);
+	  add += root -> val;
+	  sumStack.push(add);
+	  root = root -> left;
 	}
+	root = nodeStack.top();
+	add = sumStack.top();
+				 
+	if(NULL == root -> right || last == root-> right) {
+	  last = root;
+	  //deal with the case that the root-leaf path equals sum
+	  if(NULL == root -> left && NULL == root -> right && sum == add) {
+	    stack<TreeNode*> temp;
+	    while(!nodeStack.empty()) {		
+	      TreeNode* nodetemp = nodeStack.top();
+	      temp.push(nodetemp);
+	      nodeStack.pop();
+	    }
+	    while(!temp.empty()){
+	      TreeNode* nodetemp = temp.top();
+	      nodes.push_back(nodetemp->val);
+	      nodeStack.push(nodetemp);
+	      temp.pop();
+	    }
+	    all.push_back(nodes);
+	    nodes.clear();
+	  }
+	  //visit this node root
+	  nodeStack.pop();
+	  sumStack.pop();
+	  root = NULL;				   
+	}
+	else
+	  root =  root ->right;
+      }
+      // all.push_back(nodes);
+      return all;     
+    }
+  
+public:
+  TreeNode* createTree() {
+    int data;
+    TreeNode* root;
+    cin >> data;
+    if(data ==  -1)
+      root = NULL;
+    else {
+      root = (TreeNode*) malloc(sizeof(TreeNode));
+      root -> val =  data;
+      root -> left =  createTree();
+      root -> right = createTree();
+    }
+    return root;			     
+  }
 };
 
 int main(void)
 {  
-	   Solution s;
-	   TreeNode* root = s.createTree();
-	   vector< vector<int> > v = s.pathSum(root,22);
-	   vector<vector < int> >::iterator outside;
-	    vector<int>::iterator inside ;
-		for(outside = v.begin(); outside != v.end(); outside++)
-		{
-			  for(inside = outside -> begin();inside != outside ->end(); inside++)
-				    cout << *inside << " ";
-		    cout << endl;
-		}
-		cin.get();
-	   getchar();
-	   return 0;
+  Solution s;
+  TreeNode* root = s.createTree();
+  vector< vector<int> > v = s.pathSum(root,22);
+  vector<vector < int> >::iterator outside;
+  vector<int>::iterator inside ;
+  for(outside = v.begin(); outside != v.end(); outside++)
+    {
+      for(inside = outside -> begin();inside != outside ->end(); inside++)
+	cout << *inside << " ";
+      cout << endl;
+    }
+  cin.get();
+  getchar();
+  return 0;
 }
 
--- pathSum.cpp
+++ pathSum.cpp
@@ -12,85 +12,74 @@ using namespace std;
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-struct TreeNode
-{ int val;
-   TreeNode* left;
-   TreeNode* right;
-   TreeNode(int x): val(x),left(NULL),right(NULL){}
+struct TreeNode{
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode(int x): val(x),left(NULL),right(NULL){}
 };
 
 class Solution {
 public:
-    bool hasPathSum(TreeNode *root, int sum) 
-    {
-        //
-            stack<int> BSsum;
-			stack<TreeNode*> BSroot;
-			int add = 0;
-			if(NULL ==  root)
-				  return false;
-			while( NULL != root || !BSsum.empty())
-			{
-				   while(NULL!=root)
-				   {
-					      add +=  root -> val;
-						  BSroot.push(root);
-					      BSsum.push(add);
-					      root =  root ->left;
-				   }
-				       root =  BSroot.top();
-					   add= BSsum.top();
-					   if(add == sum && root ->right == NULL && root == NULL )
-					   {
-						  /* cout << " It has the nodes that adds to the sum: " << endl;
-						   while(!BSroot.empty())
-						   {
-						      cout << BSroot.top()  -> val << " + " ;
-						      BSroot.pop();
-						   }
-						   cout << endl;*/
-						    return true;
-					   }
-					   else
-						     BSsum.pop();
-					    BSroot.pop();
-						root =  root ->right;
-			}
-		//	cout<< "Dont have" << endl;
-			return false;
-        
-        //
+  bool hasPathSum(TreeNode *root, int sum) {
+    stack<int> BSsum;
+    stack<TreeNode*> BSroot;
+    int add = 0;
+    if(NULL ==  root)
+      return false;
+    while( NULL != root || !BSsum.empty()) {
+      while(NULL!=root){
+	add +=  root -> val;
+	BSroot.push(root);
+	BSsum.push(add);
+	root =  root ->left;
+      }
+      root =  BSroot.top();
+      add= BSsum.top();
+      if(add == sum && root ->right == NULL && root == NULL ){
+	/* cout << " It has the nodes that adds to the sum: " << endl;
+	   while(!BSroot.empty())
+	   {
+	   cout << BSroot.top()  -> val << " + " ;
+	   BSroot.pop();
+	   }
+	   cout << endl;*/
+	return true;
+      }
+      else
+	BSsum.pop();
+      BSroot.pop();
+      root =  root ->right;
     }
+    //	cout<< "Dont have" << endl;
+    return false;    
+    //
+  }
 
-	public:
-	TreeNode* createTree(void)
-	{    
-		  int data; 
-		  TreeNode* root;
-		  cin >> data;
-		   if( data == -1)
-			    root = NULL;
-		   else
-		   { 
-			     root = (TreeNode*)malloc(sizeof(TreeNode));
-			     root -> val = data;
-				 root -> left  = createTree();
-				 root -> right = createTree();
-		   }
-		   return root;
-	}
-
+public:
+  TreeNode* createTree(void) {    
+    int data; 
+    TreeNode* root;
+    cin >> data;
+    if( data == -1)
+      root = NULL;
+    else { 
+      root = (TreeNode*)malloc(sizeof(TreeNode));
+      root -> val = data;
+      root -> left  = createTree();
+      root -> right = createTree();
+    }
+    return root;
+  }
 };
 
 int main()
 {
-	Solution s;
-	TreeNode* root =  s.createTree();
-	   
-	bool is = s.hasPathSum(root,1);
-	cout << "Whether has sum or not: " <<  is <<  endl;
-	 
-	cin.get();
-	getchar();
-	return 0;
+  Solution s;
+  TreeNode* root =  s.createTree();	   
+  bool is = s.hasPathSum(root,1);
+  cout << "Whether has sum or not: " <<  is <<  endl;	 
+  cin.get();
+  getchar();
+  return 0;
 }
