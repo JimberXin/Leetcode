f15e2846d01923e5e410afe1245688e89cf06aa1
Junbo Xin <jimberxin@gmail.com>
2014-12-04 13:04:14 +0800

    update format of space

--- Letter Combinations of a Phone Number.cpp
+++ Letter Combinations of a Phone Number.cpp
@@ -4,61 +4,59 @@
 #include<vector>
 using namespace std;
 
-//Given a digit string, return all possible letter combinations that the number could represent.
-
-//A mapping of digit to letters (just like on the telephone buttons) is given below.
+//Given a digit string, return all possible letter combinations that
+//the number could represent.
+//A mapping of digit to letters (just like on the telephone buttons)
+//is given below.
 //Input:Digit string "23"
 //Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 class Solution {
 public:
-          map<char, string> hashTable;
+  map<char, string> hashTable;
 public:	
-	void DFS(string digits, string &path, vector<string> &result, int index){
-		    int n = digits.length();
-			if(index == n)
-			{
-				result.push_back(path);
-				return;
-			}
-
-				for(int j = 0; j < hashTable[ digits[index] ].length(); ++j)
-				{
-				     path += hashTable[ digits[index] ][j];
-				     DFS(digits, path, result, index+1);
-				     path.erase(path.end()-1);
-				}
-
-	}
-
-    vector<string> letterCombinations(string digits) {
-		  vector<string> result;
-		  string path;
-		  int n = digits.length();
-		  if(n == 0) {
-			  result.push_back(path);
-			  return result;
-		  }
-		   hashTable['2'] = "abc";
-		   hashTable['3'] = "def";
-		   hashTable['4'] = "ghi";
-		   hashTable['5'] = "jkl";
-		   hashTable['6'] = "mno";
-		   hashTable['7'] = "pqrs";
-		   hashTable['8'] = "tuv";
-		   hashTable['9'] = "wxyz";
-		   DFS(digits, path, result, 0);
-		   return result;
+  void DFS(string digits, string &path, vector<string> &result, int index){
+    int n = digits.length();
+    if(index == n){
+      result.push_back(path);
+      return;
     }
+
+    for(int j = 0; j < hashTable[ digits[index] ].length(); ++j){
+      path += hashTable[ digits[index] ][j];
+      DFS(digits, path, result, index+1);
+      path.erase(path.end()-1);
+    }
+  }
+
+  vector<string> letterCombinations(string digits) {
+    vector<string> result;
+    string path;
+    int n = digits.length();
+    if(n == 0) {
+      result.push_back(path);
+      return result;
+    }
+    hashTable['2'] = "abc";
+    hashTable['3'] = "def";
+    hashTable['4'] = "ghi";
+    hashTable['5'] = "jkl";
+    hashTable['6'] = "mno";
+    hashTable['7'] = "pqrs";
+    hashTable['8'] = "tuv";
+    hashTable['9'] = "wxyz";
+    DFS(digits, path, result, 0);
+    return result;
+  }
 };
 
 int main()
 {
-	  Solution s;
-	  string str = "";
-	  vector<string> res = s.letterCombinations(str);
-	  for(int i = 0; i < res.size(); ++i){
-		  cout << res[i] << endl;
-	  }
-	  getchar();
-	  return 0;
+  Solution s;
+  string str = "";
+  vector<string> res = s.letterCombinations(str);
+  for(int i = 0; i < res.size(); ++i){
+    cout << res[i] << endl;
+  }
+  getchar();
+  return 0;
 }
--- Longeset consetive elements.cpp
+++ Longeset consetive elements.cpp
@@ -6,38 +6,38 @@ using namespace std;
 
 class Solution {
 public:
-    int longestConsecutive(vector<int> &num) {
-          map<int,bool>  Hash;
-		  int maxNum = 0;
-		  for(int i=0; i< num.size(); i++)
-			  Hash[num[i]] = true;
-		  for(int j=0; j < num.size(); j++){
-			    if(Hash[num[j]]){
-					 Hash[num[j]] = false;
-					 int count = 1;
-					 int left = num[j] -1,  right = num[j] + 1;
-					 while( Hash.find(left) != Hash.end() ){
-						    Hash[left--] = false;
-							count ++;
-					 }
-					 while( Hash.find(right) != Hash.end()){
-						    Hash[right++] = false;
-							count++;
-					 }
-					 maxNum = max(maxNum, count);
-				}
-		  }
-		  return maxNum;
+  int longestConsecutive(vector<int> &num) {
+    map<int,bool>  Hash;
+    int maxNum = 0;
+    for(int i=0; i< num.size(); i++)
+      Hash[num[i]] = true;
+    for(int j=0; j < num.size(); j++){
+      if(Hash[num[j]]){
+	Hash[num[j]] = false;
+	int count = 1;
+	int left = num[j] -1,  right = num[j] + 1;
+	while( Hash.find(left) != Hash.end() ){
+	  Hash[left--] = false;
+	  count ++;
+	}
+	while( Hash.find(right) != Hash.end()){
+	  Hash[right++] = false;
+	  count++;
+	}
+	maxNum = max(maxNum, count);
+      }
     }
+    return maxNum;
+  }
 };
 
 int main(void){
-	  Solution s;
-	  int arr[] = {100,3,5,4,2,1,300,6};
-	  vector<int> num;
-	  for(int i = 0; i< sizeof(arr)/sizeof(int); i++)
-		  num.push_back(arr[i]);
-	  int  res = s.longestConsecutive(num);
-	  getchar();
-	  return 0;
+  Solution s;
+  int arr[] = {100,3,5,4,2,1,300,6};
+  vector<int> num;
+  for(int i = 0; i< sizeof(arr)/sizeof(int); i++)
+    num.push_back(arr[i]);
+  int  res = s.longestConsecutive(num);
+  getchar();
+  return 0;
 }
