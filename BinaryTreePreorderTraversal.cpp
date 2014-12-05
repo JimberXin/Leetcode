6eb1ecc907cfc903a8fff57fbf687eeff519be37
Junbo Xin <jimberxin@gmail.com>
2014-12-06 01:51:09 +0800

    update the format of space

--- BinaryTreePreorderTraversal.cpp
+++ BinaryTreePreorderTraversal.cpp
@@ -13,68 +13,59 @@ using namespace std;
  * };
  */
 
-struct TreeNode
-{
-	   int val;
-	   TreeNode *left;
-	   TreeNode *right;
-	   TreeNode(int x): val(x), left(NULL), right(NULL){}
+struct TreeNode{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x): val(x), left(NULL), right(NULL){}
 };
 class Solution {
 public:
-    vector<int> preorderTraversal(TreeNode *root)
-	{
-         //
-		      vector<int> values;
-			  stack<TreeNode*> BSstack;
-			 while( NULL != root || !BSstack.empty())
-			 {
-				       while(NULL != root)
-					   {
-						    // cout << root -> val << " ";
-							 values.push_back(root -> val);
-							 BSstack.push(root);
-							 root = root -> left;
-					   }
+  vector<int> preorderTraversal(TreeNode *root){
+    vector<int> values;
+    stack<TreeNode*> BSstack;
+    while( NULL != root || !BSstack.empty()){
+      while(NULL != root){
+	// cout << root -> val << " ";
+	values.push_back(root -> val);
+	BSstack.push(root);
+	root = root -> left;
+      }
 
-					     root = BSstack.top();
-						 BSstack.pop();
-						 root =  root -> right;
-			 }
-			 return values;
-
-		//
+      root = BSstack.top();
+      BSstack.pop();
+      root =  root -> right;
     }
+    return values;
+  }
 
-	public:
-	TreeNode* createTree(void)
-	{    
-		  int data; 
-		  TreeNode* root;
-		  cin >> data;
-		   if( data == -1)
-			    root = NULL;
-		   else
-		   { 
-			     root = (TreeNode*)malloc(sizeof(TreeNode));
-			     root -> val = data;
-				 root -> left  = createTree();
-				 root -> right = createTree();
-		   }
-		   return root;
-	}
-
+public:
+  TreeNode* createTree(void){    
+    int data; 
+    TreeNode* root;
+    cin >> data;
+    if( data == -1)
+      root = NULL;
+    else
+      { 
+	root = (TreeNode*)malloc(sizeof(TreeNode));
+	root -> val = data;
+	root -> left  = createTree();
+	root -> right = createTree();
+      }
+    return root;
+  }
 };
 
 int main()
 {
-	Solution s;
-	TreeNode* root =  s.createTree();
-	  vector<int> pre = s.preorderTraversal(root);
-	  for(vector<int>::iterator it = pre.begin(); it != pre.end(); it++)
-		     cout << *it << " " ;
+  Solution s;
+  TreeNode* root =  s.createTree();
+  vector<int> pre = s.preorderTraversal(root);
+  for(vector<int>::iterator it = pre.begin(); it != pre.end(); it++)
+    cout << *it << " " ;
 	 
-	cin.get();
-	getchar();
-	return 0;
+  cin.get();
+  getchar();
+  return 0;
 }
