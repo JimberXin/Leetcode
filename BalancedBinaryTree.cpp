f3c3cf9f3653b0eff8effb8827d25ab30ab5d564
Junbo Xin <jimberxin@gmail.com>
2014-12-10 00:11:13 +0800

    update the format of spcae

--- BalancedBinaryTree.cpp
+++ BalancedBinaryTree.cpp
@@ -12,103 +12,92 @@ using namespace std;
  * };
  */
 //int A[]= {1,2,3,-1,-1,4,-1,-1,5};
-struct TreeNode
-{
-	   int val;
-	   TreeNode *left;
-	   TreeNode *right;
-	   TreeNode(int x): val(x), left(NULL), right(NULL){}
+struct TreeNode{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x): val(x), left(NULL), right(NULL){}
 };
 
 class Solution {
 public:
-    bool isValidBST(TreeNode *root)
-	{
-        //
-		       return  isValidMaxMin(root, INT_MIN,INT_MAX);
-		//
+  bool isValidBST(TreeNode *root){
+    return  isValidMaxMin(root, INT_MIN,INT_MAX);
+  }
+
+public:
+  bool isValidMaxMin(TreeNode* root, int min ,int max){
+    if(NULL == root)
+      return true;
+    else if(root ->val > min &&  root ->val < max)
+      return ( isValidMaxMin(root ->left, min,root -> val)
+	    && isValidMaxMin(root ->right, root->val, max));
+    else 
+      return false;
+  }
+public:
+  TreeNode* createTree(void){    
+    int data; 
+    TreeNode* root;
+    cin >> data;
+    if( data == -1)
+      root = NULL;
+    else
+      { 
+	root = (TreeNode*)malloc(sizeof(TreeNode));
+	root -> val = data;
+	root -> left  = createTree();
+	root -> right = createTree();
+      }
+    return root;
+  }
+
+public:
+  void PreOrderTree(TreeNode *T){
+    if(NULL != T){
+      PreOrderTree(T -> left);
+      cout <<  T -> val << " ";
+      PreOrderTree(T -> right);
     }
+  }
 
 public:
-	bool isValidMaxMin(TreeNode* root, int min ,int max)
-	{
-		         if(NULL == root)
-					   return true;
-				 else if(root ->val > min &&  root ->val < max)
-					  return ( isValidMaxMin(root ->left, min,root -> val) && isValidMaxMin(root ->right, root->val, max));
-				 else 
-					   return false;
-	}
-public:
-	TreeNode* createTree(void)
-	{    
-		  int data; 
-		  TreeNode* root;
-		  cin >> data;
-		   if( data == -1)
-			    root = NULL;
-		   else
-		   { 
-			     root = (TreeNode*)malloc(sizeof(TreeNode));
-			     root -> val = data;
-				 root -> left  = createTree();
-				 root -> right = createTree();
-		   }
-		   return root;
-	}
+  void  InOrderTree(TreeNode *T){
+    if(NULL != T)
+      {
+	cout <<  T -> val << " ";
+	InOrderTree(T -> left);
+	InOrderTree(T -> right);
+      }
+  }
 
 public:
-	void PreOrderTree(TreeNode *T)
-	{
-		    if(NULL != T)
-			{
-				   PreOrderTree(T -> left);
-				   cout <<  T -> val << " ";
-				   PreOrderTree(T -> right);
-			}
-	}
-
- public:
-	void   InOrderTree(TreeNode *T)
-	{
-		    if(NULL != T)
-			{
-				   cout <<  T -> val << " ";
-				   InOrderTree(T -> left);
-				   InOrderTree(T -> right);
-			}
-	}
-
-	 public:
-	void   PostOrderTree(TreeNode *T)
-	{
-		    if(NULL != T)
-			{
-				 
-				   PostOrderTree(T -> left);
-				   PostOrderTree(T -> right);
-				     cout <<  T -> val << " ";
-			}
-	}
+  void  PostOrderTree(TreeNode *T){
+    if(NULL != T){
+      PostOrderTree(T -> left);
+      PostOrderTree(T -> right);
+      cout <<  T -> val << " ";
+    }
+  }
 };
 
 int main()
 {
-	   // { 1,2,#,#,3,#,#,4,#,5,#,#,#}
-	   Solution s;
-	  TreeNode* root = s.createTree();
-	  //cout << " PreOrder of the BST is : ";
-	  //s.PreOrderTree(root);
-	  //cout << endl << "InOrder of the BST is: ";
-	  //s.InOrderTree(root);
-	  //cout <<  endl << "PostOrder of the BST is:";
-	  //s.PostOrderTree(root);
-	  //cout << endl << root -> right -> left -> right  -> val <<  endl; //outht to be 5
-	  //cout <<  root -> left -> val  << endl;
-
-	   cout  << " whether it is a valid BST:  " <<  s.isValidBST(root) << endl;
-
-	   cin.get();
-	    getchar();
-		return 0;
+  // { 1,2,#,#,3,#,#,4,#,5,#,#,#}
+  Solution s;
+  TreeNode* root = s.createTree();
+  //cout << " PreOrder of the BST is : ";
+  //s.PreOrderTree(root);
+  //cout << endl << "InOrder of the BST is: ";
+  //s.InOrderTree(root);
+  //cout <<  endl << "PostOrder of the BST is:";
+  //s.PostOrderTree(root);
+  //cout << endl << root->right->left->right->val << endl; //outht to be 5
+  //cout <<  root -> left -> val  << endl;
+  
+  cout  << " whether it is a valid BST:  " <<  s.isValidBST(root) << endl;
+  
+  cin.get();
+  getchar();
+  return 0;
 }
