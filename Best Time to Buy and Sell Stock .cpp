6ea38369581fbe356d3a539b3d4d5804fbdc69b4
Junbo Xin <jimberxin@gmail.com>
2014-12-17 12:15:43 +0800

    add problem description

--- Best Time to Buy and Sell Stock .cpp
+++ Best Time to Buy and Sell Stock .cpp
@@ -1,3 +1,15 @@
+//Last Modified: 2014/12/17
+//Author: Junbo Xin
+
+/*
+//*********************************Problem Description*********************************
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction 
+(ie, buy one and sell one share of the stock),
+ design an algorithm to find the maximum profit.
+*/
+
 #include<iostream>
 #include<vector>
 using namespace std;
