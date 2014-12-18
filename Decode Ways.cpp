b40ffc5b39a5659944759a8270e1d470cc94c3ac
Junbo Xin <jimberxin@gmail.com>
2014-12-19 01:44:47 +0800

    add problem description

--- Decode Ways.cpp
+++ Decode Ways.cpp
@@ -1,3 +1,24 @@
+//Last Modified:  2014/12/19
+//Author:  Junbo Xin
+
+/*
+*************************************Problem Description**************************************
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+Given an encoded message containing digits, determine the total number of ways to decode it.
+
+For example,
+Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
+
+The number of ways decoding "12" is 2.
+*/
+
+
+
 #include<iostream>
 #include<vector>
 #include<string>
@@ -15,7 +36,7 @@ public:
       {
 	if(s[i] == '0')
 	  dp[i] = 0;
-	else if(s[i] == '1' || (s[i] == '2' &&  s[i+1] <= '6'))
+	else if(s[i] == '1' || (s[i] == '2' && s[i+1] <= '6'))
 	  {
 	    if( i == n -2)
 	      dp[i] = dp[i+1] + 1;
