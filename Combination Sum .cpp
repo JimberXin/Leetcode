26fa7b3836161e3400a7a01df69aa35a944eaef5
Junbo Xin <jimberxin@gmail.com>
2014-12-18 17:03:14 +0800

    add problem description

--- Combination Sum .cpp
+++ Combination Sum .cpp
@@ -1,3 +1,22 @@
+//Last Modified: 2014/12/18
+//Author: Junbo Xin
+
+/*
+Given a set of candidate numbers (C) and a target number (T), 
+find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.
+
+Note:
+All numbers (including target) will be positive integers.
+Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+The solution set must not contain duplicate combinations.
+For example, given candidate set 2,3,6,7 and target 7, 
+A solution set is: 
+[7] 
+[2, 2, 3] 
+*/
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
@@ -16,14 +35,14 @@ public:
   }
 
   void DFS(vector<int> nums, int gap, int current, vector<int> &temp, vector<vector<int> > &result){
-    //   find onde solution
+    // find onde solution
     if(gap == 0){ 
       result.push_back(temp);
       return;
     }
     for(size_t i=current;i<nums.size();i++){
       if(gap < nums[i])
-	return ;  // cut it down
+	return;  // cut it down
       temp.push_back(nums[i]);
       DFS(nums,gap-nums[i],i,temp,result);
       temp.pop_back();
