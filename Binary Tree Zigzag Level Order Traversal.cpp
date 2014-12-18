8d13ce31bde0f4b32c8f3549d8d6b1d9c824baa4
Junbo Xin <jimberxin@gmail.com>
2014-12-18 16:55:11 +0800

    add problem description

--- Binary Tree Zigzag Level Order Traversal.cpp
+++ Binary Tree Zigzag Level Order Traversal.cpp
@@ -1,8 +1,25 @@
-#include<iostream>
-#include<vector>
-#include<queue>
-using namespace std;
-/**
+//Last Modified: 2014/12/18
+//Author: Junbo Xin
+
+/*
+******************************Problem Description**************************************
+Given a binary tree, return the zigzag level order traversal of its nodes' values.
+ (ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+
  * Definition for binary tree
  * struct TreeNode {
  *     int val;
@@ -10,11 +27,19 @@ using namespace std;
  *     TreeNode *right;
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
- */
+
+*/
+
+
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
 struct TreeNode{  
   int val;
   TreeNode *left;
-  TreeNode* right;
+  TreeNode *right;
   TreeNode(int x): val(x),left(NULL), right(NULL){}
 };
 
@@ -26,29 +51,29 @@ public:
     queue<TreeNode*> nodes;
     int curnum =1;
     int nextnum = 0;
-    if(NULL == root)
+    if(root == NULL)
       return all;
     nodes.push(root);
-    pair.push_back(root -> val);
+    pair.push_back(root->val);
     all.push_back(pair);
     pair.clear();
-    while( ! nodes.empty()){
+    while(!nodes.empty()){
       root = nodes.front();   // delEnqueue
       nodes.pop();
       curnum --;
 
-      if(NULL != root ->left){
-	nodes.push(root -> left);
-	pair.push_back( root->left ->val);
+      if(root->left != NULL){
+	nodes.push(root->left);
+	pair.push_back(root->left ->val);
 	nextnum ++;
       }
-      if(NULL != root->right){
-	nodes.push(root -> right);
-	pair.push_back(root ->right ->val);
+      if(root->right != NULL){
+	nodes.push(root->right);
+	pair.push_back(root->right->val);
 	nextnum++;
       }
 
-      if( 0 == curnum){
+      if(0 == curnum){
 	if(!pair.empty())
 	  all.push_back(pair);
 	pair.clear();
@@ -60,7 +85,7 @@ public:
     int flag = 1;
     for(it = all.begin();it != all.end(); it++){
       if(flag == -1)
-	reverse(it ->begin(),it->end());
+	reverse(it->begin(),it->end());
       flag *= -1;
     }
     return all; 
@@ -71,12 +96,12 @@ public:
     TreeNode* root;
     int data;
     cin >> data;
-    if( data == -1)
+    if(data == -1)
       root = NULL;
     else{
-      root =  new TreeNode(data);
-      root -> left   =  createTree();
-      root -> right =  createTree();			 
+      root = new TreeNode(data);
+      root->left  = createTree();
+      root->right = createTree();			 
     }
     return root;
   }
