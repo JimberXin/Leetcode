8cc6e9973e9abd3ecc91aa416501cd41f2b7c810
Junbo Xin <jimberxin@gmail.com>
2014-12-20 00:04:17 +0800

    add problem description

--- Evaluate Reverse Polish Notation.cpp
+++ Evaluate Reverse Polish Notation.cpp
@@ -1,3 +1,17 @@
+//Last Modified:   2014/12/20
+//Author:  Junbo Xin
+
+/*
+*************************Problem Description****************************************
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+Some examples:
+  ["2", "1", "+", "3", "*" ] -> ((2 + 1) * 3) -> 9
+  ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+*/
+
 #include<iostream>
 #include<vector>
 #include<string>
