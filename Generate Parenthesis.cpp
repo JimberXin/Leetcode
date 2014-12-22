d3292432511ff4854625d30ddde8a3f81d0f4c73
Junbo Xin <jimberxin@gmail.com>
2014-12-22 12:15:39 +0800

    add problem description

--- Generate Parenthesis.cpp
+++ Generate Parenthesis.cpp
@@ -1,3 +1,16 @@
+//Last Modified:  2014/12/22
+//Author:   Junbo Xin
+
+/*
+*************************************Problem Description**************************
+Given n pairs of parentheses, write a function to generate all combinations of 
+well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+"((()))", "(()())", "(())()", "()(())", "()()()"
+*/
+
 #include<iostream>
 #include<vector>
 #include<string>
