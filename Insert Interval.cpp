b0906078ce594f22e20c176f52b215fd2bce2d7e
Junbo Xin <jimberxin@gmail.com>
2014-12-22 12:22:41 +0800

    add problem description

--- Insert Interval.cpp
+++ Insert Interval.cpp
@@ -1,10 +1,22 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
+//Last Modified:  2014/12/22
+//Author:  Junbo Xin
 
-using namespace std;
+/*
+***********************************Problem Description*****************************
+Given a set of non-overlapping intervals, insert a new interval into the intervals 
+(merge if necessary).
 
-/**
+You may assume that the intervals were initially sorted according to their start times.
+
+Example 1:
+Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
+
+Example 2:
+Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
+
+This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+
+
  * Definition for an interval.
  * struct Interval {
  *     int start;
@@ -12,7 +24,14 @@ using namespace std;
  *     Interval() : start(0), end(0) {}
  *     Interval(int s, int e) : start(s), end(e) {}
  * };
- */
+*/
+
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
 
 struct Interval 
 {
@@ -71,7 +90,7 @@ public:
 	result.push_back(*it);
 	continue;
       }
-      if(it -> start  >  newInterval.end){
+      if(it->start > newInterval.end){
 	if(flag){
 	  result.push_back(newInterval);
 	  flag = false;
@@ -79,8 +98,8 @@ public:
 	result.push_back(*it);
 	continue;			     
       }
-      newInterval.start =  it->start < newInterval.start? it-> start: newInterval.start;
-      newInterval.end  =  it -> end > newInterval.end? it->end: newInterval.end;
+      newInterval.start =  it->start < newInterval.start? it->start: newInterval.start;
+      newInterval.end  =   it->end > newInterval.end? it->end: newInterval.end;
     }
     if(flag)
       result.push_back(newInterval);
