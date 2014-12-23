5257bc663169bc34f9844cc29ff287a4848106ed
Junbo Xin <jimberxin@gmail.com>
2014-12-23 09:43:51 +0800

    add problem description

--- Letter Combinations of a Phone Number.cpp
+++ Letter Combinations of a Phone Number.cpp
@@ -1,15 +1,25 @@
+//Last Modified:  2014/12/23
+//Author：  Junbo Xin
+
+/*
+*********************Problem Description*************************
+Given a digit string, return all possible letter combinations that 
+the number could represent.
+A mapping of digit to letters (just like on the telephone buttons)
+is given below.
+Input:Digit string "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+Note:
+Although the above answer is in lexicographical order, 
+your answer could be in any order you want.
+*/
+
 #include<iostream>
 #include<string>
 #include<map>
 #include<vector>
 using namespace std;
 
-//Given a digit string, return all possible letter combinations that
-//the number could represent.
-//A mapping of digit to letters (just like on the telephone buttons)
-//is given below.
-//Input:Digit string "23"
-//Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 class Solution {
 public:
   map<char, string> hashTable;
@@ -21,7 +31,7 @@ public:
       return;
     }
 
-    for(int j = 0; j < hashTable[ digits[index] ].length(); ++j){
+    for(int j = 0; j < hashTable[digits[index]].length(); ++j){
       path += hashTable[ digits[index] ][j];
       DFS(digits, path, result, index+1);
       path.erase(path.end()-1);
