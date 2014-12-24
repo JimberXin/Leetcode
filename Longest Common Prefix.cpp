739f19fb5e46ea653acb3c6991d73974ec1831cf
Junbo Xin <jimberxin@gmail.com>
2014-12-24 16:34:47 +0800

    add problem description

--- Longest Common Prefix.cpp
+++ Longest Common Prefix.cpp
@@ -1,3 +1,10 @@
+//Last Modified: 2014/12/24
+//Author:  Junbo Xin
+
+/*
+**********************************Problem Description********************************
+Write a function to find the longest common prefix string amongst an array of strings
+*/
 #include<iostream>
 #include<vector>
 #include<string>
