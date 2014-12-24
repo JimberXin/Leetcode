5cd406634d7915fcd27e4a2278dfac572d18c42d
Junbo Xin <jimberxin@gmail.com>
2014-12-24 16:39:52 +0800

    add problem description

--- Longest Palindrome Substring.cpp
+++ Longest Palindrome Substring.cpp
@@ -1,3 +1,13 @@
+//Last Modified:  2014/12/24
+//Author:  Junbo Xin
+
+/*
+*****************Problem Description***************************
+Given a string S, find the longest palindromic substring in S.
+You may assume that the maximum length of S is 1000, 
+and there exists one unique longest palindromic substring.
+*/
+
 #include<iostream>
 #include<vector>
 #include<string>
