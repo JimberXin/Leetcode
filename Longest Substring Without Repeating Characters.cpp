8b43ce59ff6d5d39d6c7f404ff834d32873031cd
Junbo Xin <jimberxin@gmail.com>
2014-12-25 20:29:51 +0800

    add problem description

--- Longest Substring Without Repeating Characters.cpp
+++ Longest Substring Without Repeating Characters.cpp
@@ -1,3 +1,13 @@
+//Last Modified:  2014/12/25
+//Author: Junbo Xin
+
+/*
+**********************************Problem Description********************************
+Given a string, find the length of the longest substring without repeating characters. 
+For example, the longest substring without repeating letters for "abcabcbb" is "abc", 
+which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+*/
+
 #include<iostream>
 #include<string>
 #include<algorithm>
@@ -14,19 +24,19 @@ public:
     memset(position,0,sizeof(position));
     for(int i=0; i< s.size();i++) {
       //*****Already exist, delect all the char before where it emerges
-      if(flag[ s[i]  - ' ' ] == true)
+      if(flag[s[i]-' '])
 	{
-	  for(int j=start; j< position[ s[i] - ' ' ]; j++)
-	    flag[ s[j] - ' ' ] = false;
-	  start = position[  s[i] - ' ' ] + 1;
-	  position[ s[i] - ' ' ] = i;
-	  flag[ s[i] - ' ' ] = true;
+	  for(int j=start; j < position[s[i]-' ']; j++)
+	    flag[s[j]-' '] = false;
+	  start = position[s[i]-' '] + 1;
+	  position[s[i]-' '] = i;
+	  flag[s[i]-' '] = true;
 	}
       //***************go on traversal*********************
       else
 	{
-	  position[ s[i] - ' ' ] = i;
-	  flag[ s[i] - ' ' ] = true;
+	  position[s[i]-' '] = i;
+	  flag[s[i]-' '] = true;
 	  maxLength = max(maxLength, i-start +1);
 	}
     }
